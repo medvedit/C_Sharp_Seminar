4ccc547927ea780ce705bca0bd94ae1ea386c19e
medvedit <medwed.sa.job@gmail.com>
2022-10-02 21:21:06 +0300

    Начало домашки

--- Seminar_001/Program.cs
+++ Seminar_001/Program.cs
@@ -128,6 +128,5 @@ else
 // Console.Write(count);
 // Console.WriteLine("\"");
 
-                        // Домашняя работа
+                                            // Домашняя работа
 
-                        
