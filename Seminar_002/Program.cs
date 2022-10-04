66d23d4136d4e79e61d0c6b16d7a2a0f445f8f79
medvedit <medwed.sa.job@gmail.com>
2022-10-04 22:56:29 +0300

    solved the example №10

--- Seminar_002/Program.cs
+++ Seminar_002/Program.cs
@@ -1,1 +1,18 @@
 ﻿
+// int namber = new Random().Next(10, 100);
+// int nam1 = namber/10; // 72 ->
+
+Console.WriteLine("----------Задача №10----------");
+// Напишите программу, которая на вход принимает трёхзначеное число
+// и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трёхначеное число: ");
+int number = int.Parse(Console.ReadLine()!);
+if((number < 100) || (number >= 1000))
+{
+     Console.WriteLine($"Число {number} не является трёхзначеным!");
+}
+if((number >= 100) && (number < 1000))
+{
+    int number1 = number%100/10;
+    Console.WriteLine($"Вторая цифра введенного Вами числа {number} равна {number1}");
+}
