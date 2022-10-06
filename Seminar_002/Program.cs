d42541a615a31922a1f3a75f25a9897026816477
medvedit <medwed.sa.job@gmail.com>
2022-10-06 11:58:16 +0300

    add решение Задача 5 в классе

--- Seminar_002/Program.cs
+++ Seminar_002/Program.cs
@@ -1,8 +1,7 @@
 ﻿// //Задача1 в классе.-------------------------
 // // напишите программу, которая выводит случайное число из отрезка [10,.. 99]
-// // и выводит наибольшее число.
+// // и выводит наибольшее число. Пример: 72 -> 7
 // int namber = new Random().Next(10, 100); //Если в .Next(100) указать одно число, будет вывод в диапазоне от 0 до 99. Можно указать число .Next(-10,-1)
-// // 72 -> 7
 // Console.WriteLine(namber);
 // int nam1 = namber / 10; //72 => 7,2 => 7
 // int nam2 = namber % 10; // 72 => 7,2 => 2
@@ -73,94 +72,104 @@
 // int square1 = int.Parse(Console.ReadLine()!);
 // Console.WriteLine("Введите первое число: ");
 // int square2 = int.Parse(Console.ReadLine()!);
-// if(square1 > square2){
-//     if(square1 == Math.Pow(square2,2)){
-//     Console.WriteLine($"Число {square1} является квадратом числа {square2}");
-//     }else{
-//       Console.WriteLine($"Число {square1}  не является квадратом числа {square2}");  
-//     }
-// }  else if (square1 < square2){
-//     if(square2 == Math.Pow(square1,2)){
-//     Console.WriteLine($"Число {square2} является квадратом числа {square1}");
-//     }else{
-//       Console.WriteLine($"Число {square2}  не является квадратом числа {square1}");  
-//     }
-// }    
+// if((square1 % square2 == 0) || (square2 % square1 == 0))
+// {
+//     Console.WriteLine("Yes");
+// }                                                          
+// else{
+//     Console.WriteLine("No");
+// }     
+
+//     // if(square1 > square2){
+//     //     if(square1 == Math.Pow(square2, 2)){
+//     //     Console.WriteLine($"Число {square1} является квадратом числа {square2}");
+//     //     }else{
+//     //       Console.WriteLine($"Число {square1}  не является квадратом числа {square2}");  
+//     //     }
+//     // }  else if (square1 < square2){
+//     //     if(square2 == Math.Pow(square1, 2)){
+//     //     Console.WriteLine($"Число {square2} является квадратом числа {square1}");
+//     //     }else{
+//     //       Console.WriteLine($"Число {square2}  не является квадратом числа {square1}");  
+//     //     }
+//     // }                                                   
+
+
 //// && - логическое "И" Должны выполняться оба уловия между которыми стоят эти два амперсанта.
 //// || - логическое "ИЛИ" Выполняется одно из условий. Верно, когда хотя бы одно из условий верно.
 
 
 
 //_________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
-Console.WriteLine("----------Задача №10----------");
-// Напишите программу, которая на вход принимает трёхзначеное число
-// и на выходе показывает вторую цифру этого числа.
+// Console.WriteLine("----------Задача №10----------");
+// // Напишите программу, которая на вход принимает трёхзначеное число
+// // и на выходе показывает вторую цифру этого числа.
 
-Console.WriteLine("Введите трёхначеное число: ");
-int num = int.Parse(Console.ReadLine()!);
-if((num < 100) || (num >= 1000))
-{
-     Console.WriteLine($"Число {num} не является трёхзначеным!");
-}
-if((num >= 100) && (num < 1000))
-{
-    int num1 = num % 100 / 10;
-    Console.WriteLine($"Вторая цифра введенного Вами числа {num} равна {num1}");
-}
+// Console.WriteLine("Введите трёхначеное число: ");
+// int num = int.Parse(Console.ReadLine()!);
+// if((num < 100) || (num >= 1000))
+// {
+//      Console.WriteLine($"Число {num} не является трёхзначеным!");
+// }
+// if((num >= 100) && (num < 1000))
+// {
+//     int num1 = num % 100 / 10;
+//     Console.WriteLine($"Вторая цифра введенного Вами числа {num} равна {num1}");
+// }
 
-Console.WriteLine("----------Задача №13----------");
-////Напишите задачу, которая выводит третью цифру заданного числа или сообщяет, 
-//// что третьей цифры нет. (Максимальгое число 100.000).
+// Console.WriteLine("----------Задача №13----------");
+// ////Напишите задачу, которая выводит третью цифру заданного числа или сообщяет, 
+// //// что третьей цифры нет. (Максимальгое число 100.000).
 
-int number = new Random().Next(1, 100001);
-if(number < 100)
-{
-    Console.WriteLine($"Случайное число {number}");
-    Console.WriteLine($"В числе {number} третья цифра отсутствует.");
-}
-if((number >= 100) && (number < 1000))
-{
-    int number1 = number % 10;
-    Console.WriteLine($"Случайное число {number}");
-    Console.WriteLine($"Третья цыфра числа {number} равна {number1}");
-}
-if((number >= 1000) && (number < 10000))
-{
-    int number1 = number % 100 / 10;
-    Console.WriteLine($"Случайное число {number}");
-    Console.WriteLine($"Третья цыфра числа {number} равна {number1}");
-}
-if((number >= 10000) && (number < 100000))
-{
-    int number1 = number % 1000 / 100;
-    Console.WriteLine($"Случайное число {number}");
-    Console.WriteLine($"Третья цыфра числа {number} равна {number1}");
-}
-if((number >= 100000) && (number < 100001))
-{
-    int number1 = number % 10000 / 1000;
-    Console.WriteLine($"Случайное число {number}");
-    Console.WriteLine($"Третья цыфра числа {number} равна {number1}");
-}
+// int number = new Random().Next(1, 100001);
+// if(number < 100)
+// {
+//     Console.WriteLine($"Случайное число {number}");
+//     Console.WriteLine($"В числе {number} третья цифра отсутствует.");
+// }
+// if((number >= 100) && (number < 1000))
+// {
+//     int number1 = number % 10;
+//     Console.WriteLine($"Случайное число {number}");
+//     Console.WriteLine($"Третья цыфра числа {number} равна {number1}");
+// }
+// if((number >= 1000) && (number < 10000))
+// {
+//     int number1 = number % 100 / 10;
+//     Console.WriteLine($"Случайное число {number}");
+//     Console.WriteLine($"Третья цыфра числа {number} равна {number1}");
+// }
+// if((number >= 10000) && (number < 100000))
+// {
+//     int number1 = number % 1000 / 100;
+//     Console.WriteLine($"Случайное число {number}");
+//     Console.WriteLine($"Третья цыфра числа {number} равна {number1}");
+// }
+// if((number >= 100000) && (number < 100001))
+// {
+//     int number1 = number % 10000 / 1000;
+//     Console.WriteLine($"Случайное число {number}");
+//     Console.WriteLine($"Третья цыфра числа {number} равна {number1}");
+// }
 
-Console.WriteLine("----------Задача №15----------");
-// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
-// и проверяет, является ли этот день выходным. 
-// Обязательно сделать проверку на ввод чисел меньше 1 и больше 7.
+// Console.WriteLine("----------Задача №15----------");
+// // Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// // и проверяет, является ли этот день выходным. 
+// // Обязательно сделать проверку на ввод чисел меньше 1 и больше 7.
 
-Console.Write("Введите номер дня недели: ");
-int dayweek = int.Parse(Console.ReadLine()!);
-if((dayweek < 1) || (dayweek > 7))
-{
-    Console.WriteLine($"Введённое Вами число {dayweek} не является номером дня недели. В неделе семь дней. ");
+// Console.Write("Введите номер дня недели: ");
+// int dayweek = int.Parse(Console.ReadLine()!);
+// if((dayweek < 1) || (dayweek > 7))
+// {
+//     Console.WriteLine($"Введённое Вами число {dayweek} не является номером дня недели. В неделе семь дней. ");
     
-} 
-if((dayweek >= 1) && (dayweek <= 5))
-{
-    Console.WriteLine($"Введённый Вами день недели является рабочим днём.");
+// } 
+// if((dayweek >= 1) && (dayweek <= 5))
+// {
+//     Console.WriteLine($"Введённый Вами день недели является рабочим днём.");
 
-}
-if((dayweek >= 6) && (dayweek <= 7))
-{
-    Console.WriteLine($"Введённый Вами день недели является выходным днем днём. GO!!! писать код)))))");
-}
+// }
+// if((dayweek >= 6) && (dayweek <= 7))
+// {
+//     Console.WriteLine($"Введённый Вами день недели является выходным днем днём. GO!!! писать код)))))");
+// }
