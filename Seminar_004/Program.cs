1c0af61c543a3d16ed9267b60bfe20d6053fde25
medvedit <medwed.sa.job@gmail.com>
2022-10-13 20:32:45 +0300

    Задача 27, домашка

--- Seminar_004/Program.cs
+++ Seminar_004/Program.cs
@@ -163,3 +163,36 @@
 //     int result = Convert.ToInt32(Math.Pow(x, y));
 //     return result;
 // }
+
+// Задача 27:--------------------------
+// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в числе {A} равна {NumberSum(A)}");
+
+int NumberSum (int number)
+{
+    int result = 0;
+
+    for (int i = 0; i < number; i++)
+    {
+    result = result + number % 10;
+    number = number / 10;
+    }
+    
+    return result;
+}
+
+
+
+
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
