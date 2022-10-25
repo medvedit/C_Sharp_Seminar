949e1b4ce3e66f0cfe445a63fb2850ae6e615d9f
medvedit <medwed.sa.job@gmail.com>
2022-10-25 22:03:23 +0300

    task 47

--- Seminar_007/Program.cs
+++ Seminar_007/Program.cs
@@ -10,31 +10,31 @@
 // int[,] array = GetArray(rows, columns, 0, 50);
 // PrintArray(array);
 
-int [,] GetArray (int m, int n, int minValue, int maxValue)
-{
-    int [,] result = new int[m,n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            result[i,j] = new Random().Next(minValue, maxValue + 1);
-        }
-    }
-    return result;
-}
+// int [,] GetArray (int m, int n, int minValue, int maxValue)
+// {
+//     int [,] result = new int[m,n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i,j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return result;
+// }
 
-void PrintArray(int [,] Array)
-{
-    for (int i = 0; i < Array.GetLength(0); i++) // GetLength обрашение к размеру, (0) - строки.
-    {
-        for (int j = 0; j < Array.GetLength(1); j++) // GetLength обрашение к размеру, (1) - столбцы.
-        {
-            Console.Write($"{Array[i,j]} ");
-        }
-        Console.WriteLine();
-    }
+// void PrintArray(int [,] Array)
+// {
+//     for (int i = 0; i < Array.GetLength(0); i++) // GetLength обрашение к размеру, (0) - строки.
+//     {
+//         for (int j = 0; j < Array.GetLength(1); j++) // GetLength обрашение к размеру, (1) - столбцы.
+//         {
+//             Console.Write($"{Array[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
     
-}
+// }
 
 // Задача №2 в классе:__________________________________________________________
 // Задайте двумерный массив размера m на n, 
@@ -173,29 +173,86 @@ void PrintArray(int [,] Array)
 //Задайте двумерный массив. Найдите сумму элементов, 
 //находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
 
+// Console.WriteLine("Введите колличество строк: ");
+// int rows = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите колличество столбцов: ");
+// int columns = int.Parse(Console.ReadLine()!);
+
+// int[,] array = GetArray(rows, columns, 2, 5);
+// Console.WriteLine();
+// PrintArray(array);
+// Console.WriteLine();
+// Console.WriteLine($"Сумма всех элементов на  \"главной диагонали\" =  {SumOfElements(array)}");
+// Console.WriteLine();
+
+
+// int SumOfElements(int[,]sumElementsArray)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < sumElementsArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < sumElementsArray.GetLength(1); j++)
+//         {
+//             if(i == j){
+//                 sum = sum + sumElementsArray[i,j];
+//             }
+//         }
+//     }return sum;
+// }
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------ДОМАШНЯЯ РАБОТА--------------------------------------------------------------------------------------------------------------------------------
+
+
+// Задача 47: -------------------------------------------------------------------------------
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
 Console.WriteLine("Введите колличество строк: ");
 int rows = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите колличество столбцов: ");
 int columns = int.Parse(Console.ReadLine()!);
 
-int[,] array = GetArray(rows, columns, 2, 5);
-Console.WriteLine();
+double[,] array = GetArray(rows, columns);
 PrintArray(array);
-Console.WriteLine();
-Console.WriteLine($"Сумма всех элементов на  \"главной диагонали\" =  {SumOfElements(array)}");
-Console.WriteLine();
 
 
-int SumOfElements(int[,]sumElementsArray)
+double[,] GetArray (int m, int n)
 {
-    int sum = 0;
-    for (int i = 0; i < sumElementsArray.GetLength(0); i++)
+    Random rnd = new Random();
+    double[,] result = new double[m,n];
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < sumElementsArray.GetLength(1); j++)
+        for (int j = 0; j < n; j++)
         {
-            if(i == j){
-                sum = sum + sumElementsArray[i,j];
-            }
+            result[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/ 10.0);
         }
-    }return sum;
+    }
+    return result;
 }
+
+void PrintArray(double [,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < Array.GetLength(1); j++) 
+        {
+            Console.Write($"{Array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+// Ответ: Введите колличество строк: 
+//        3
+//        Введите колличество столбцов: 
+//        4
+//        9,4 -8,5 -9,5 -0,3 
+//        -2,4 4,1 2,9 1,9 
+//        0,1 -3,3 -3,9 9,5 
+
+
+
+
