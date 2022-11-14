895e375bc5194a48135da528f0dddf9767a10104
medvedit <medwed.sa.job@gmail.com>
2022-11-14 14:11:03 +0300

    task 58

--- Seminar_008/Program.cs
+++ Seminar_008/Program.cs
@@ -333,42 +333,114 @@
 // //        50 47 33 30 27 
 // //        42 29 24 19 17 
 
-// Задача 56:______________________________________________________________________________________________________________________
-// Задайте прямоугольный двумерный массив. 
-// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// // Задача 56:______________________________________________________________________________________________________________________
+// // Задайте прямоугольный двумерный массив. 
+// // Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Console.Write("Введите колличество строк: ");
+// int rows = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите колличество столбцов: ");
+// int columns = int.Parse(Console.ReadLine()!);
+// int[,] array = GetArray(rows, columns, 1, 10);
+
+// PrintArray(array);
+// Console.WriteLine();
+// Console.WriteLine($"Строка с наименьшей суммой - {SumRows(array)}");
+
+// int SumRows(int[,] arr)
+// {
+//     int minrow = 0;
+//     int minsum = 0;
+//     for (int i = 0; i < arr.GetLength(1); i++)
+//     {
+//         minsum += arr[0, i];
+//     }
+//     for (int i = 1; i < arr.GetLength(0); i++)
+//     {
+//         int sum = 0;
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             sum += arr[i, j];
+//         }
+//         if (minsum > sum)
+//         {
+//             minsum = sum;
+//             minrow = i+1;
+//         }
+//     }
+//     return minrow;
+// }
+
+// int[,] GetArray(int m, int n, int minValue, int maxValue)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] Array)
+// {
+//     for (int i = 0; i < Array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < Array.GetLength(1); j++)
+//         {
+//             Console.Write($"{Array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// // Ответ: Введите колличество строк: 3
+// //        Введите колличество столбцов: 4
+// //        9 4 7 4 
+// //        9 7 1 4 
+// //        10 5 10 4 
+
+// // Строка с наименьшей суммой - 2
+
+
+// Задача 58:__________________________________________________________________________________________________________________
+// Задайте две квадратные матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
 Console.Write("Введите колличество строк: ");
 int rows = int.Parse(Console.ReadLine()!);
 Console.Write("Введите колличество столбцов: ");
 int columns = int.Parse(Console.ReadLine()!);
-int[,] array = GetArray(rows, columns, 1, 10);
-
-PrintArray(array);
 Console.WriteLine();
-Console.WriteLine($"Строка с наименьшей суммой - {SumRows(array)}");
-
-int SumRows(int[,] arr)
+if(columns != rows)
 {
-    int minrow = 0;
-    int minsum = 0;
-    for (int i = 0; i < arr.GetLength(1); i++)
+    Console.WriteLine("Число строк должно быть равным числу столбцов");
+    return;
+}
+
+int[,] arrayA = GetArray(rows, columns, 1, 3);
+int[,]arrayB = GetArray(rows, columns, 1, 3);
+
+PrintArray(arrayA);
+Console.WriteLine();
+PrintArray(arrayB);
+Console.WriteLine();
+PrintArray(SumMatrix(arrayA,arrayB));
+
+int[,] SumMatrix(int[,]array1, int[,]array2)
+{
+    int[,]array3 = new int[array1.GetLength(0),array2.GetLength(1)];
+    for (int i = 0; i < array1.GetLength(0); i++)
     {
-        minsum += arr[0, i];
+       for (int j = 0; j < array2.GetLength(1); j++)
+       {
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+                array3[i,j] = array3[i,j] + array1[i,k] * array2[k,j];  //C[i,j] = a*C[i,j] + b*Sum(A[i,k]*B[k,j]) формула!!!
+            }
+       } 
     }
-    for (int i = 1; i < arr.GetLength(0); i++)
-    {
-        int sum = 0;
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            sum += arr[i, j];
-        }
-        if (minsum > sum)
-        {
-            minsum = sum;
-            minrow = i+1;
-        }
-    }
-    return minrow;
+    return array3;
 }
 
 int[,] GetArray(int m, int n, int minValue, int maxValue)
@@ -395,11 +467,3 @@ void PrintArray(int[,] Array)
         Console.WriteLine();
     }
 }
-// Ответ: Введите колличество строк: 3
-//        Введите колличество столбцов: 4
-//        9 4 7 4 
-//        9 7 1 4 
-//        10 5 10 4 
-
-// Строка с наименьшей суммой - 2
-
