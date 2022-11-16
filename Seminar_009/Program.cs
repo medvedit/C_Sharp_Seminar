37a7738771f09c4328d34e9fc8060dac3a50c658
medvedit <medwed.sa.job@gmail.com>
2022-11-16 16:04:10 +0300

    task 2 in class

--- Seminar_009/Program.cs
+++ Seminar_009/Program.cs
@@ -1,11 +1,24 @@
-﻿// Задание №1:___________________________________________________________________________
-// Разбор работы рекурсии на основе вычесления факториала.
-// n = 1*2*3*4
-int f = 4;
-Console.WriteLine(Factorial(f));
-int Factorial(int n)
+﻿// // Задание №1:___________________________________________________________________________
+// // Разбор работы рекурсии на основе вычесления факториала.
+// // n = 1*2*3*4
+// int f = 4;
+// Console.WriteLine(Factorial(f));
+// int Factorial(int n)
+// {
+//     if (n == 1) return 1; // как только n == 1 , программа остановит выполнение, выход.
+//     return n * Factorial(n - 1); //n * Factorial(3), т.е. на выходе 4 * 3 * 2 * 1
+// }
+// // Ответ: 24
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumebs(1,N));
+
+string PrintNumebs(int start, int end)
 {
-    if (n == 1) return 1; // как только n == 1 , программа остановит выполнение, выход.
-    return n * Factorial(n - 1); //n * Factorial(3), т.е. на выходе 4 * 3 * 2 * 1
+    if(start == end) return start.ToString();
+    return (start + ", " + PrintNumebs(start+1, end));
 }
-// Ответ: 24
+// Ответ: Введите число N: 5
+//        1, 2, 3, 4, 5
