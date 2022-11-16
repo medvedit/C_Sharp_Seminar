0ed07763e7d393fda432af746301a05b31ecee0a
medvedit <medwed.sa.job@gmail.com>
2022-11-16 23:11:01 +0300

    task 69 in class

--- Seminar_009/Program.cs
+++ Seminar_009/Program.cs
@@ -25,21 +25,61 @@
 // // Ответ: Введите число N: 5
 // //        1, 2, 3, 4, 5
 
-// Задача №65:
-// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N с помощью рекурсии.
-Console.Write("Введите число M: ");
-int M = int.Parse(Console.ReadLine()!);
-Console.Write("Введите число N: ");
-int N = int.Parse(Console.ReadLine()!);
+// // Задача №65:_________________________________________________________________________________
+// // Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N с помощью рекурсии.
+// Console.Write("Введите число M: ");
+// int M = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число N: ");
+// int N = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine(PrintNumebs(M, N));
+// Console.WriteLine(PrintNumebs(M, N));
 
-string PrintNumebs(int start, int end)
+// string PrintNumebs(int start, int end)
+// {
+//     if (start == end) return start.ToString();
+//     return (start + ", " + PrintNumebs(start + 1, end));
+// }
+// // Ответ: Введите число M: 4
+// //        Введите число N: 8
+// //        4, 5, 6, 7, 8
+
+
+// // Задача 67:____________________________________________________________________________________
+// // Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+// Console.Write("Введите число N: ");
+// int N = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine(SumNumber(N));
+
+// int SumNumber(int n)
+// {
+//     if (n < 1)
+//     {
+//         return n;
+//     }
+//     return n % 10 + SumNumber(n / 10);
+// }
+// Ответ: Введите число N: 4512
+//        12
+
+// Задача 69:____________________________________________________________________________________
+// Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"A = {A}; B = {B} -> {DegreeNumber(A, B)}");
+
+int DegreeNumber(int a, int b)
 {
-    if (start == end) return start.ToString();
-    return (start + ", " + PrintNumebs(start + 1, end));
+    if (b == 1) return a;
+    return a * DegreeNumber(a, b - 1);
 }
-// Ответ: Введите число M: 4
-//        Введите число N: 8
-//        4, 5, 6, 7, 8
+// Ответ: Введите число A: 2
+//        Введите число B: 3
+//        A = 2; B = 3 -> 8
 
